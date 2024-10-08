34faac50562b469a5c678d8e17fa89899e7e7c5b
rania <boubrikrania1@gmail.com>
2024-10-08 23:04:23 +0200

    feat: add basic MenuScreen composable
    - Implement a basic MenuScreen composable that displays a simple text message.
    - Currently shows "This is the Menu screen" as a placeholder.

---
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/overview/MenuScreen.kt
@@ -0,0 +1,13 @@
+package com.github.lookupgroup27.lookup.ui.overview
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+class MenuScreen {
+
+    @Composable
+    fun MenuScreen() {
+        Text(text = "This is the Menu screen.")
+    }
+
+}
