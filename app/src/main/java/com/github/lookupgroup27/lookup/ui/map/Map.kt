24f09e551150617ef088a043bca5051f590b7caa
rania <boubrikrania1@gmail.com>
2024-10-10 18:10:41 +0200

    refactor: Convert Map and Menu from classes to composables
    - Refactor MapScreen and MenuScreen from class-based components to composable functions
    - Improved alignment with Jetpack Compose architecture and best practices

---
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/map/Map.kt
@@ -0,0 +1,11 @@
+package com.github.lookupgroup27.lookup.ui.map
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+
+    @Composable
+    fun MapScreen() {
+        Text(text = "This is the Map screen.")
+    }
--- app/src/main/java/com/github/lookupgroup27/lookup/ui/overview/MenuScreen.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/overview/MenuScreen.kt
@@ -3,10 +3,10 @@ package com.github.lookupgroup27.lookup.ui.overview
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-class MenuScreen {
+
 
   @Composable
   fun MenuScreen() {
     Text(text = "This is the Menu screen.")
   }
-}
+
