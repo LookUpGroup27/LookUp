1e3a9962ad82df905f119edf20fa35bdad4c4c60
Kenzoud <kenza.oudrhiribenaddach@epfl.ch>
2024-10-11 19:10:46 +0200

    chore: add missing Profile and SkyTracker composable files

---
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/skytracker/SkyTracker.kt
@@ -0,0 +1,6 @@
+package com.github.lookupgroup27.lookup.ui.skytracker
+
+import androidx.compose.runtime.Composable
+import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
+
+@Composable fun SkyTrackerScreen(navigationActions: NavigationActions) {}
