8ee430bf6c86ca185afd6ae9d0e4812c712ef49a
rania <boubrikrania1@gmail.com>
2024-10-12 14:50:23 +0200

    chore: apply ktfmt formatting

--- app/src/main/java/com/github/lookupgroup27/lookup/ui/map/Map.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/map/Map.kt
@@ -3,9 +3,7 @@ package com.github.lookupgroup27.lookup.ui.map
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-
-
-    @Composable
-    fun MapScreen() {
-        Text(text = "This is the Map screen.")
-    }
+@Composable
+fun MapScreen() {
+  Text(text = "This is the Map screen.")
+}
--- app/src/main/java/com/github/lookupgroup27/lookup/ui/overview/Menu.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/overview/Menu.kt
@@ -3,10 +3,7 @@ package com.github.lookupgroup27.lookup.ui.overview
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
-
-
-  @Composable
-  fun MenuScreen() {
-    Text(text = "This is the Menu screen.")
-  }
-
+@Composable
+fun MenuScreen() {
+  Text(text = "This is the Menu screen.")
+}
