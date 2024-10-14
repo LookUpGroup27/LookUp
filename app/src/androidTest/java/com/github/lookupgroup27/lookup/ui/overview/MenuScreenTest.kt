ccbeba52bebd6a2c7391b9479208f24a7a8a43c8
Rihab <rihbelmekki@gmail.com>
2024-10-14 19:25:24 +0200

    test: add navigation tests for Menu and Map tabs in BottomNavigationMenu

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/MenuScreenTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/MenuScreenTest.kt
@@ -21,6 +21,32 @@ class MenuScreenTest {
   }
 
   @Test
+  fun menuScreen_bottomNavigation_clickMenuTab_navigatesToMenu() {
+    composeTestRule.setContent { MenuScreen(navigationActions = mockNavigationActions) }
+
+    // Click on the Menu tab
+    composeTestRule.onNodeWithTag("Menu").performClick()
+    // Find the correct TopLevelDestination for "Map"
+    val menuDestination = LIST_TOP_LEVEL_DESTINATION.first { it.textId == "Menu" }
+
+    // Verify that navigation to the Map screen is triggered with the correct object
+    verify(mockNavigationActions).navigateTo(menuDestination)
+  }
+
+  @Test
+  fun menuScreen_bottomNavigation_clickMapTab_navigatesToMap() {
+    composeTestRule.setContent { MenuScreen(navigationActions = mockNavigationActions) }
+
+    // Click on the Map tab
+    composeTestRule.onNodeWithTag("Map").performClick()
+
+    val mapDestination = LIST_TOP_LEVEL_DESTINATION.first { it.textId == "Map" }
+
+    // Verify that navigation to the Map screen is triggered with the correct object
+    verify(mockNavigationActions).navigateTo(mapDestination)
+  }
+
+  @Test
   fun menuScreen_displaysWelcomeText() {
     composeTestRule.setContent { MenuScreen(navigationActions = mockNavigationActions) }
 
