d281eed996ddf2cc68d8af920cedec1825f4d42f
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-10-15 23:58:08 +0200

    test: update CalendarScreenTest to mock IcalRepository and test with StateFlow

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/CalendarScreenTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/CalendarScreenTest.kt
@@ -1,39 +1,59 @@
 package com.github.lookupgroup27.lookup.ui.overview
 
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
 import com.github.lookupgroup27.lookup.model.calendar.CalendarViewModel
+import com.github.lookupgroup27.lookup.model.calendar.MockIcalRepository
 import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
-import io.mockk.mockk
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class CalendarScreenTest {
 
+  @get:Rule val composeTestRule = createComposeRule()
+
   private lateinit var calendarViewModel: CalendarViewModel
   private lateinit var navigationActions: NavigationActions
+  private lateinit var navController: TestNavHostController
   private val dateFormat = SimpleDateFormat("MMMM yyyy", Locale.getDefault())
-
-  @get:Rule val composeTestRule = createComposeRule()
 
   @Before
   fun setUp() {
 
-    calendarViewModel = mockk(relaxed = true)
-    navigationActions = mockk(relaxed = true)
+    val mockIcalData =
+        """
+            BEGIN:VCALENDAR
+            BEGIN:VEVENT
+            DTSTART:20251001T120000Z
+            DTEND:20251001T130000Z
+            SUMMARY:Test Event
+            END:VEVENT
+            END:VCALENDAR
+        """
+
+    val mockIcalRepository = MockIcalRepository(mockIcalData)
+    calendarViewModel = CalendarViewModel(mockIcalRepository)
 
     composeTestRule.setContent {
+      navController = TestNavHostController(LocalContext.current)
+      navController.navigatorProvider.addNavigator(ComposeNavigator())
+      navigationActions = NavigationActions(navController)
       CalendarScreen(calendarViewModel = calendarViewModel, navigationActions = navigationActions)
     }
   }
 
   @Test
-  fun testCalendarHeaderDisplaysCurrentMonth() {
+  fun testCalendarHeaderDisplaysCurrentMonth() = runTest {
     val currentDate = Calendar.getInstance().time
     val formattedMonth = dateFormat.format(currentDate)
 
@@ -41,7 +61,7 @@ class CalendarScreenTest {
   }
 
   @Test
-  fun testPreviousMonthButton() {
+  fun testPreviousMonthButton() = runTest {
     val currentDate = Calendar.getInstance().time
     val previousMonthDate =
         Calendar.getInstance()
@@ -52,12 +72,12 @@ class CalendarScreenTest {
             .time
     val formattedPreviousMonth = dateFormat.format(previousMonthDate)
 
-    composeTestRule.onNodeWithText("<").performClick()
+    composeTestRule.onNodeWithContentDescription("Previous Month").performClick()
     composeTestRule.onNodeWithText(formattedPreviousMonth).assertIsDisplayed()
   }
 
   @Test
-  fun testNextMonthButton() {
+  fun testNextMonthButton() = runTest {
     val currentDate = Calendar.getInstance().time
     val nextMonthDate =
         Calendar.getInstance()
@@ -68,19 +88,17 @@ class CalendarScreenTest {
             .time
     val formattedNextMonth = dateFormat.format(nextMonthDate)
 
-    composeTestRule.onNodeWithText(">").performClick()
+    composeTestRule.onNodeWithContentDescription("Next Month").performClick()
     composeTestRule.onNodeWithText(formattedNextMonth).assertIsDisplayed()
   }
 
   @Test
-  fun testLookUpEventButtonExists() {
-    composeTestRule.onNodeWithText("Look Up Event").assertIsDisplayed()
+  fun testLookUpEventButtonExists() = runTest {
+    composeTestRule.onNodeWithContentDescription("Look Up Event").assertIsDisplayed()
   }
 
-  // Add more tests when figma is updated with all the components and testTags
-
   @Test
-  fun testSelectingDayUpdatesSelectedDate() {
+  fun testSelectingDayUpdatesSelectedDate() = runTest {
     val calendar = Calendar.getInstance()
     val dayToSelect = calendar.get(Calendar.DAY_OF_MONTH) + 1
 
