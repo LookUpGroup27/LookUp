382fd72edf5a328a2dc436c516f1cad48caa481a
Ismaillat <ismail.lataoui@epfl.ch>
2024-10-17 23:15:09 +0200

    fix: change composeTestRule initialization

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/LandingKtTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/LandingKtTest.kt
@@ -3,6 +3,7 @@ package com.github.lookupgroup27.lookup.ui.overview
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -10,16 +11,22 @@ import androidx.compose.ui.test.performClick
 import androidx.test.espresso.intent.Intents
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.lookupgroup27.lookup.MainActivity
+import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
+import com.github.lookupgroup27.lookup.ui.navigation.Screen
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 
 @RunWith(AndroidJUnit4::class)
 class LandingKtTest {
 
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule val composeTestRule = createComposeRule()
+
+  private val mockNavigationActions: NavigationActions = mock()
 
   @Before
   fun setUp() {
@@ -35,6 +42,8 @@ class LandingKtTest {
 
   @Test
   fun logoAndButtonAreDisplayed() {
+    composeTestRule.setContent { LandingScreen(mockNavigationActions) }
+
     // Verify that the Look Up logo is displayed
     composeTestRule.onNodeWithContentDescription("Look Up Logo").assertIsDisplayed()
 
@@ -45,6 +54,8 @@ class LandingKtTest {
 
   @Test
   fun backgroundIsClickableAndNavigatesToMap() {
+    composeTestRule.setContent { LandingScreen(mockNavigationActions) }
+
     // Verify that the background image is displayed and clickable
     composeTestRule.onNodeWithContentDescription("Background").assertIsDisplayed()
     composeTestRule.onNodeWithContentDescription("Background").assertHasClickAction()
@@ -56,11 +67,13 @@ class LandingKtTest {
     composeTestRule.waitForIdle()
 
     // Assert that the Map screen is displayed by checking for specific text or UI elements
-    composeTestRule.onNodeWithTag("map_screen").assertIsDisplayed()
+    verify(mockNavigationActions).navigateTo(Screen.MAP)
   }
 
   @Test
   fun homeButtonIsClickable() {
+    composeTestRule.setContent { LandingScreen(mockNavigationActions) }
+
     // Perform click action on the home button using its testTag
     composeTestRule.onNodeWithTag("Home Icon").performClick()
 
@@ -68,11 +81,13 @@ class LandingKtTest {
     composeTestRule.waitForIdle()
 
     // Assert that the Menu screen is displayed by checking for specific text
-    composeTestRule.onNodeWithTag("menu_screen").assertIsDisplayed()
+    verify(mockNavigationActions).navigateTo(Screen.MENU)
   }
 
   @Test
   fun testMapViewPromptIsDisplayed() {
+    composeTestRule.setContent { LandingScreen(mockNavigationActions) }
+
     // Assert that it is displayed
     composeTestRule.onNodeWithText("Click for full map view").assertIsDisplayed()
   }
