f6189381724b882623050fbf1bc27fa68da5f231
Adrien Bousquie <adrien.bousquie@epfl.ch>
2024-10-17 17:53:21 +0200

    test: add test to the new composable
    Added test for the QuizOptionButton function in the Quiz.kt file

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/quiz/QuizKtTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/quiz/QuizKtTest.kt
@@ -7,11 +7,11 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
 import com.github.lookupgroup27.lookup.ui.navigation.Screen
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
 
 class QuizKtTest {
 
@@ -19,26 +19,31 @@ class QuizKtTest {
 
   private val mockNavigationActions: NavigationActions = mock()
 
-  @Before
   fun setUp() {
     composeTestRule.setContent { QuizScreen(navigationActions = mockNavigationActions) }
   }
 
   @Test
   fun quizScreen_displaysCorrectly() {
+    setUp()
     composeTestRule.onNodeWithTag("quiz_screen").assertIsDisplayed()
     composeTestRule.onNodeWithTag("go_back_button_quiz").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("quiz_background").assertIsDisplayed()
 
     // Check the title is correctly displayed
+    composeTestRule.onNodeWithTag("quiz_title").assertIsDisplayed()
     composeTestRule.onNodeWithText("Take a Quiz").assertIsDisplayed()
 
     // Check the 2 themes are correctly displayed
+    composeTestRule.onNodeWithTag("earth_button").assertIsDisplayed()
     composeTestRule.onNodeWithText("Earth").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("solar_system_button").assertIsDisplayed()
     composeTestRule.onNodeWithText("Solar System").assertIsDisplayed()
   }
 
   @Test
   fun quizScreen_clickBackButton_navigatesBack() {
+    setUp()
     // Perform click on the back button
     composeTestRule.onNodeWithTag("go_back_button_quiz").performClick()
 
@@ -48,6 +53,7 @@ class QuizKtTest {
 
   @Test
   fun quizScreen_earthButtonRedirectCorrectly() {
+    setUp()
     // Perform click on the Earth button
     composeTestRule.onNodeWithText("Earth").performClick()
 
@@ -57,10 +63,46 @@ class QuizKtTest {
 
   @Test
   fun quizScreen_solarButtonRedirectCorrectly() {
+    setUp()
     // Perform click on the Earth button
     composeTestRule.onNodeWithText("Solar System").performClick()
 
     // Verify navigation to Earth quiz action is triggered
     verify(mockNavigationActions).navigateTo(Screen.QUIZ_PLAY)
+  }
+
+  @Test
+  fun quizOptionButton_behavesNormally() {
+    composeTestRule.setContent {
+      QuizOptionButton(
+          text = "Earth",
+          onClick = { mockNavigationActions.navigateTo(Screen.QUIZ_PLAY) },
+          testTag = "earth_button")
+    }
+
+    composeTestRule.onNodeWithText("Earth").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("earth_button").assertIsDisplayed()
+
+    // Perform click on the Earth button
+    composeTestRule.onNodeWithTag("earth_button").performClick()
+
+    // Verify navigation to Earth quiz action is triggered
+    verify(mockNavigationActions).navigateTo(Screen.QUIZ_PLAY)
+  }
+
+  @Test
+  fun quizOptionButton_doesNothingOnClickEmpty() {
+    composeTestRule.setContent {
+      QuizOptionButton(text = "Solar System", onClick = {}, testTag = "solar_system_button")
+    }
+
+    composeTestRule.onNodeWithText("Solar System").assertIsDisplayed()
+    composeTestRule.onNodeWithTag("solar_system_button").assertIsDisplayed()
+
+    // Perform click on the Solar System button
+    composeTestRule.onNodeWithTag("solar_system_button").performClick()
+
+    // Verify navigation to Solar System quiz action is triggered
+    verifyNoInteractions(mockNavigationActions)
   }
 }
