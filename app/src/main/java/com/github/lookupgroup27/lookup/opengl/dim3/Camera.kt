2cb46a94d1a1b26431d390a41e821f1e09d1140c
Adrien Bousquie <adrien.bousquie@epfl.ch>
2024-11-03 18:11:25 +0100

    feat(opengl-tutorial): implement basic camera

--- app/src/main/java/com/github/lookupgroup27/lookup/opengl/dim3/Camera.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/opengl/dim3/Camera.kt
@@ -1,4 +1,30 @@
 package com.github.lookupgroup27.lookup.opengl.dim3
 
-class Camera {
+import android.icu.text.Transliterator.Position
+import android.opengl.Matrix
+
+class Camera (var position: FloatArray, var width: Int, var height: Int) {
+  var orientation: FloatArray = floatArrayOf(0f, 0f, -1f)
+  var up: FloatArray = floatArrayOf(0f, 1f, 0f)
+
+  var ratio = width.toFloat() / height.toFloat()
+
+  var speed: Float = 0.1f
+
+  fun Matrix(FOVdeg: Float, nearPlane: Float, farPlane: Float) {
+    var view = FloatArray(16)
+    var projection = FloatArray(16)
+    Matrix.setIdentityM(view, 0)
+    Matrix.setIdentityM(projection, 0)
+
+    Matrix.setLookAtM(view, 0, position[0], position[1], position[2], position[0] + orientation[0], position[1] + orientation[1], position[2] + orientation[2], up[0], up[1], up[2])
+    Matrix.perspectiveM(projection, 0, FOVdeg, ratio, nearPlane, farPlane)
+
+
+  }
+
+  fun Inputs() {
+
+  }
+
 }
