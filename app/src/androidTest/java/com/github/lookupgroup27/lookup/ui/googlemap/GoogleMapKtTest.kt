eee5cb1da6b99f35eb8323e9f3e43f11025ba74e
Kenzoud <kenza.oudrhiribenaddach@epfl.ch>
2024-11-03 18:12:46 +0100

    style: apply ktfmt format to code

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/googlemap/GoogleMapKtTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/googlemap/GoogleMapKtTest.kt
@@ -1,119 +1,10 @@
 package com.github.lookupgroup27.lookup.ui.googlemap
 
-// toDo: try to correctly write the test for this class
-
-/*import android.location.Location
-import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createComposeRule
-import com.github.lookupgroup27.lookup.ui.map.MapScreen
-import com.github.lookupgroup27.lookup.ui.navigation.LIST_TOP_LEVEL_DESTINATION
-import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
-import org.junit.*
-import org.mockito.kotlin.*
-
-class GoogleMapKtTest {
-
-    @get:Rule val composeTestRule = createComposeRule()
-
-    private val mockNavigationActions: NavigationActions = mock()
-
-    @Test
-    fun mapScreen_displaysBackgroundImage() {
-        composeTestRule.setContent { GoogleMapScreen(navigationActions = mockNavigationActions) }
-
-        // Verify the background image is displayed
-        composeTestRule.onNodeWithTag("googleMapScreen").assertIsDisplayed()
-    }
-
-    /*@Test
-    fun menuScreen_displaysBottomNavigationMenu() {
-        composeTestRule.setContent { GoogleMapScreen(navigationActions = mockNavigationActions) }
-
-        // Check that the bottom navigation menu is displayed
-        composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
-    }*/
-
-    @Test
-    fun testMarkerIsDisplayedWhenLocationPermissionGranted() {
-        // Mock location
-        val mockLocation = Location("provider").apply {
-            latitude = 37.4219983
-            longitude = -122.084
-        }
-
-        // Set the Compose content with mocked location and permission
-        composeTestRule.setContent {
-            GoogleMapScreen(navigationActions = mockNavigationActions)
-        }
-
-        // Check if the marker is displayed
-        composeTestRule.onNodeWithText("You are here").assertIsDisplayed()
-    }
-}*/
-
-/*import androidx.compose.ui.test.assertHasClickAction
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.test.espresso.intent.Intents
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.GrantPermissionRule
-import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
-import com.github.lookupgroup27.lookup.ui.navigation.Screen
-import com.github.lookupgroup27.lookup.ui.overview.LandingScreen
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
-import android.Manifest
-
-@RunWith(AndroidJUnit4::class)
-class GoogleMapScreenTest {
-
-    @get:Rule val composeTestRule = createComposeRule()
-    @get:Rule
-    val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION)
-
-
-    private val mockNavigationActions: NavigationActions = mock()
-
-    @Before
-    fun setUp() {
-        // Initialize Espresso Intents to capture and validate outgoing intents
-        Intents.init()
-    }
-
-    @After
-    fun tearDown() {
-        // Release Espresso Intents after tests
-        Intents.release()
-    }
-
-    @Test
-    fun logoAndButtonAreDisplayed() {
-        composeTestRule.setContent { GoogleMapScreen(mockNavigationActions) }
-
-        // Verify that the GoogleMapScreen is displayed
-        composeTestRule.onNodeWithTag("googleMapScreen").assertIsDisplayed()
-
-        // Ensure the bottom navigation is set up correctly
-        composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
-    }
-
-}*/
-
 import android.Manifest
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.rule.GrantPermissionRule
-import com.github.lookupgroup27.lookup.model.location.LocationProvider
 import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
 import com.github.lookupgroup27.lookup.ui.navigation.Screen
 import com.google.android.gms.maps.model.LatLng
@@ -126,7 +17,6 @@ import org.mockito.Mockito.`when`
 class GoogleMapScreenTest {
 
   private lateinit var navigationActions: NavigationActions
-  private lateinit var mockLocationProvider: LocationProvider
 
   @get:Rule val composeTestRule = createComposeRule()
 
@@ -154,25 +44,6 @@ class GoogleMapScreenTest {
     // Ensure the bottom navigation is set up correctly
     composeTestRule.onNodeWithTag("bottomNavigationMenu").assertIsDisplayed()
   }
-
-  /*@Test
-  fun testMarkerIsDisplayedWhenLocationPermissionGranted() {
-      // Mock location
-      val mockLocation = Location("provider").apply {
-          latitude = 37.4219983
-          longitude = -122.084
-      }
-
-      // Create a real instance of LocationProvider
-      val context = ApplicationProvider.getApplicationContext<Context>()
-      mockLocationProvider = LocationProvider(context).apply {
-          currentLocation.value = mockLocation
-      }
-
-      // Check if the marker is displayed
-      composeTestRule.onNodeWithText("You are here").assertIsDisplayed()
-
-      }*/
 
   @Test
   fun mapIsCenteredOnCurrentLocation() {
