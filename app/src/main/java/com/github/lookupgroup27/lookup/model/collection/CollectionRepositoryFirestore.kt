1a64a97fd0054689df7cfd59234dc0a7456dd9c6
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-11 09:44:45 +0100

    fix(collection): enhance getUserImageUrls function with better error handling and detailed logging
    - Refined getUserImageUrls function to handle cases where the user email might be null or empty
    - Added detailed logging at each step of image retrieval to aid in debugging and improve traceability
    - Improved error handling within the function to ensure graceful failure when Firebase operations encounter issues

--- app/src/main/java/com/github/lookupgroup27/lookup/model/collection/CollectionRepositoryFirestore.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/collection/CollectionRepositoryFirestore.kt
@@ -1,4 +1,39 @@
 package com.github.lookupgroup27.lookup.model.collection
 
-class CollectionRepositoryFirestore {
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.coroutines.tasks.await
+
+class CollectionRepositoryFirestore(
+    private val db: FirebaseStorage,
+    private val auth: FirebaseAuth
+) : CollectionRepository {
+
+  override fun init(onSuccess: () -> Unit) {
+    auth.addAuthStateListener {
+      if (it.currentUser != null) {
+        onSuccess()
+      }
+    }
+  }
+
+  override suspend fun getUserImageUrls(): List<String> {
+    val userEmail = auth.currentUser?.email ?: ""
+    if (userEmail.isEmpty()) {
+      Log.e("FirebaseCollectionRepo", "User email is empty.")
+      return emptyList()
+    }
+
+    val folderPath = "images/$userEmail/"
+    val imagesRef = db.reference.child(folderPath)
+
+    return try {
+      val result = imagesRef.listAll().await()
+      result.items.mapNotNull { it.downloadUrl.await().toString() }
+    } catch (e: Exception) {
+      Log.e("FirebaseCollectionRepo", "Error retrieving images from Firebase Storage", e)
+      emptyList()
+    }
+  }
 }
