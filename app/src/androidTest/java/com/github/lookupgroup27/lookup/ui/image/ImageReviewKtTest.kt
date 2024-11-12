893c1f4ed2e1adf3fa7f9c2fccfe94aea9b375ed
Kenzoud <kenza.oudrhiribenaddach@epfl.ch>
2024-11-12 21:38:19 +0100

    test(image-review): inject postsViewModel to ImageReviewScreen

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/image/ImageReviewKtTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/image/ImageReviewKtTest.kt
@@ -5,34 +5,55 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.lookupgroup27.lookup.model.post.PostsRepository
 import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
 import com.github.lookupgroup27.lookup.ui.navigation.Screen
+import com.github.lookupgroup27.lookup.ui.post.PostsViewModel
 import java.io.File
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
 @RunWith(AndroidJUnit4::class)
 class ImageReviewTest {
 
+  private lateinit var postsViewModel: PostsViewModel
+  private lateinit var postsRepository: PostsRepository
+
   @get:Rule val composeTestRule = createComposeRule()
 
-  val fakeFile: File = File.createTempFile("temp", null)
+  private val fakeFile: File = File.createTempFile("temp", null)
 
   private val mockNavigationActions: NavigationActions = mock()
 
+  @Before
+  fun setUp() {
+    postsRepository = Mockito.mock(PostsRepository::class.java)
+    postsViewModel = PostsViewModel(postsRepository)
+
+    // Mock UID generator to return a fixed value
+    `when`(postsViewModel.generateNewUid()).thenReturn("mocked_uid")
+  }
+
   @Test
   fun testImageReviewIsDisplayed() {
-    composeTestRule.setContent { ImageReviewScreen(mockNavigationActions, fakeFile) }
+    composeTestRule.setContent {
+      ImageReviewScreen(mockNavigationActions, fakeFile, postsViewModel)
+    }
 
     composeTestRule.onNodeWithTag("image_review").assertIsDisplayed()
   }
 
   @Test
   fun testConfirmButtonIsDisplayedAndClickable() {
-    composeTestRule.setContent { ImageReviewScreen(mockNavigationActions, fakeFile) }
+    composeTestRule.setContent {
+      ImageReviewScreen(mockNavigationActions, fakeFile, postsViewModel)
+    }
 
     composeTestRule.onNodeWithTag("confirm_button").assertIsDisplayed()
     composeTestRule.onNodeWithTag("confirm_button").performClick()
@@ -40,11 +61,28 @@ class ImageReviewTest {
 
   @Test
   fun testCancelButtonIsDisplayedAndClickable() {
-    composeTestRule.setContent { ImageReviewScreen(mockNavigationActions, fakeFile) }
+    composeTestRule.setContent {
+      ImageReviewScreen(mockNavigationActions, fakeFile, postsViewModel)
+    }
 
     composeTestRule.onNodeWithTag("cancel_button").assertIsDisplayed()
     composeTestRule.onNodeWithTag("cancel_button").performClick()
 
     verify(mockNavigationActions).navigateTo(Screen.TAKE_IMAGE)
   }
+
+  /*@Test
+  fun testConfirmButtonNavigatesAfterSavingImage() {
+    composeTestRule.setContent { ImageReviewScreen(mockNavigationActions, fakeFile,postsViewModel) }
+
+    // Click on the "Save Image" button
+    composeTestRule.onNodeWithTag("confirm_button").performClick()
+
+    // Wait for Compose to complete the interactions
+    composeTestRule.waitForIdle()
+
+    // Verify that the navigation to Google Map happens after clicking save
+    verify(mockNavigationActions).navigateTo(Screen.GOOGLE_MAP)
+  }*/
+
 }
