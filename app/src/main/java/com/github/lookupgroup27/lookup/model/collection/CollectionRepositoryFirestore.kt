279adb40319c68bb0acae37b3772a82b0f8140b4
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-12 06:30:40 +0100

    feat: add documentation as recommanded in milestone1

--- app/src/main/java/com/github/lookupgroup27/lookup/model/collection/CollectionRepositoryFirestore.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/collection/CollectionRepositoryFirestore.kt
@@ -5,11 +5,25 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.tasks.await
 
+/**
+ * CollectionRepositoryFirestore is responsible for fetching image URLs associated with a user's
+ * email from Firebase Storage. This implementation utilizes Firebase Authentication and Storage
+ * services to retrieve user-specific data.
+ *
+ * @param db The FirebaseStorage instance used to access storage data.
+ * @param auth The FirebaseAuth instance used for user authentication.
+ */
 class CollectionRepositoryFirestore(
     private val db: FirebaseStorage,
     private val auth: FirebaseAuth
 ) : CollectionRepository {
 
+  /**
+   * Initializes the repository by checking the authentication state of the user. If the user is
+   * authenticated, the provided [onSuccess] callback is called.
+   *
+   * @param onSuccess A callback to execute when the user is authenticated.
+   */
   override fun init(onSuccess: () -> Unit) {
     auth.addAuthStateListener {
       if (it.currentUser != null) {
@@ -18,6 +32,12 @@ class CollectionRepositoryFirestore(
     }
   }
 
+  /**
+   * Retrieves a list of image URLs associated with the authenticated user's email from Firebase
+   * Storage. If the user is not authenticated or if an error occurs, an empty list is returned.
+   *
+   * @return A list of image URLs associated with the user's email.
+   */
   override suspend fun getUserImageUrls(): List<String> {
     val userEmail = auth.currentUser?.email ?: ""
     if (userEmail.isEmpty()) {
