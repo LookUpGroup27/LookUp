dc4d5d6ff0a37629227c8c5985c2aa5cbb3acaf2
Kenzoud <kenza.oudrhiribenaddach@epfl.ch>
2024-11-14 17:57:58 +0100

    refactor: create "components" package for MapView, MapControls and AddMapMarker composables

---
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/googlemap/components/MapControls.kt
@@ -0,0 +1,30 @@
+package com.github.lookupgroup27.lookup.ui.googlemap.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.github.lookupgroup27.lookup.ui.theme.*
+
+/**
+ * Map controls for toggling auto centering
+ *
+ * @param autoCenteringEnabled Whether auto centering is enabled
+ * @param onCenteringToggle The callback to be called when the auto centering is toggled
+ */
+@Composable
+fun MapControls(autoCenteringEnabled: Boolean, onCenteringToggle: (Boolean) -> Unit) {
+  Box(modifier = Modifier.fillMaxWidth().background(DarkBlue).padding(16.dp)) {
+    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+      Button(onClick = { onCenteringToggle(true) }) { Text(text = "Auto Center On") }
+      Button(onClick = { onCenteringToggle(false) }) { Text(text = "Auto Center Off") }
+    }
+  }
+}
