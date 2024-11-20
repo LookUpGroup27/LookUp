6b6f5220dd64327cd926e990415d972c17024351
Kenzoud <kenza.oudrhiribenaddach@epfl.ch>
2024-11-20 13:58:52 +0100

    feat(sky-map): update the Renderer class
    - Add an instance of the SkyBox class.
    - Initialize and render the SkyBox within the OpenGL rendering lifecycle methods.

--- app/src/main/java/com/github/lookupgroup27/lookup/model/map/Renderer.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/map/Renderer.kt
@@ -2,10 +2,12 @@ package com.github.lookupgroup27.lookup.model.map
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import com.github.lookupgroup27.lookup.model.map.renderables.Object
 import com.github.lookupgroup27.lookup.model.map.renderables.Star
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
+import com.github.lookupgroup27.lookup.model.map.skybox.SkyBox
 
 /**
  * Provides the OpenGL rendering logic for the GLSurfaceView. This class is responsible for drawing
@@ -14,9 +16,15 @@ import javax.microedition.khronos.opengles.GL10
 class Renderer : GLSurfaceView.Renderer {
 
   private lateinit var shapes: List<Object>
+  private lateinit var skyBox: SkyBox
 
   /** The camera used to draw the shapes on the screen. */
   val camera = Camera()
+
+  // Temporary storage for the MVP matrix
+  private val mvpMatrix = FloatArray(16)
+  private val viewMatrix = FloatArray(16)
+  private val projectionMatrix = FloatArray(16)
 
   override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
     // Set the background frame color
@@ -24,24 +32,42 @@ class Renderer : GLSurfaceView.Renderer {
 
     GLES20.glEnable(GLES20.GL_DEPTH_TEST)
 
+      // Initialize the SkyBox
+      skyBox = SkyBox()
+      skyBox.initialize()
+
+    // Initialize the camera's matrices
+    Matrix.setIdentityM(viewMatrix, 0)
+    Matrix.setIdentityM(projectionMatrix, 0)
+
     // Create the shapes (Make sure you always create the shapes after the OpenGL context is
     // created)
-    shapes =
+    /*shapes =
         listOf(
             Star(0.0f, 0f, -1f, floatArrayOf(1.0f, 1.0f, 1.0f)),
             Star(0.24f, 0f, -0.97f, floatArrayOf(1.0f, 1.0f, 1.0f)),
             Star(1f, 0f, 0f, color = floatArrayOf(1.0f, 0.0f, 0.0f)),
             Star(0f, 1f, 0f, color = floatArrayOf(0.0f, 1.0f, 0.0f)),
-            Star(0f, 0f, 1f, color = floatArrayOf(0.0f, 0.0f, 1.0f)))
+            Star(0f, 0f, 1f, color = floatArrayOf(0.0f, 0.0f, 1.0f))) */
   }
 
   override fun onDrawFrame(unused: GL10) {
-    // Redraw background color
+    /*// Redraw background color
     GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-    GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT)
+    GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT)*/
 
-    // Draw the shapes
-    for (shape in shapes) shape.draw(camera)
+      // Clear the screen
+      GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
+
+    // Calculate the MVP matrix (Model-View-Projection matrix)
+    Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+      // Render the SkyBox
+      skyBox.render(mvpMatrix)
+
+
+      // Draw the shapes
+    //for (shape in shapes) shape.draw(camera)
   }
 
   override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
