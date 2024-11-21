f2a14c3847afa1ad7cff2304dcd9dedfb65a1f9e
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-21 03:26:27 +0100

    feature(register): implement Firebase-backed repository for user registration

--- app/src/main/java/com/github/lookupgroup27/lookup/model/register/RegisterRepositoryFirestore.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/register/RegisterRepositoryFirestore.kt
@@ -1,13 +1,12 @@
-package com.github.lookupgroup27.lookup.data.repository
+package com.github.lookupgroup27.lookup.model.register
 
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
 
-class RegisterRepository {
+class RegisterRepositoryFirestore(private val auth: FirebaseAuth = FirebaseAuth.getInstance()) :
+    RegisterRepository {
 
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
-    suspend fun registerUser(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password).await()
-    }
+  override suspend fun registerUser(email: String, password: String) {
+    auth.createUserWithEmailAndPassword(email, password).await()
+  }
 }
