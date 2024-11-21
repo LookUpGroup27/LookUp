607f0d30d03befe9273975b88d18cb4d78794b79
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-21 03:21:22 +0100

    test(register): write UI tests for register screen to validate components and navigation flow

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/register/RegisterKtTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/register/RegisterKtTest.kt
@@ -1,4 +1,77 @@
 package com.github.lookupgroup27.lookup.ui.register
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.navigation.NavHostController
+import androidx.test.core.app.ApplicationProvider
+import com.github.lookupgroup27.lookup.model.register.RegisterRepository
+import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
+import org.junit.Rule
+import org.junit.Test
+
+class MockRegisterRepository : RegisterRepository {
+  override suspend fun registerUser(email: String, password: String) {}
+}
+
 class RegisterKtTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  private fun createMockViewModel(): RegisterViewModel {
+    val repository = MockRegisterRepository()
+    return RegisterViewModel(repository)
+  }
+
+  private fun mockNavigationActions() =
+      NavigationActions(
+          navController = NavHostController(ApplicationProvider.getApplicationContext()))
+
+  @Test
+  fun appLogo_isDisplayed() {
+    composeTestRule.setContent {
+      RegisterScreen(viewModel = createMockViewModel(), navigationActions = mockNavigationActions())
+    }
+    composeTestRule.onNodeWithTag("app_logo").assertIsDisplayed()
+  }
+
+  @Test
+  fun screenTitle_isDisplayed() {
+    composeTestRule.setContent {
+      RegisterScreen(viewModel = createMockViewModel(), navigationActions = mockNavigationActions())
+    }
+    composeTestRule.onNodeWithTag("screen_title").assertIsDisplayed()
+  }
+
+  @Test
+  fun emailField_isDisplayed() {
+    composeTestRule.setContent {
+      RegisterScreen(viewModel = createMockViewModel(), navigationActions = mockNavigationActions())
+    }
+    composeTestRule.onNodeWithTag("email_field").assertIsDisplayed()
+  }
+
+  @Test
+  fun passwordField_isDisplayed() {
+    composeTestRule.setContent {
+      RegisterScreen(viewModel = createMockViewModel(), navigationActions = mockNavigationActions())
+    }
+    composeTestRule.onNodeWithTag("password_field").assertIsDisplayed()
+  }
+
+  @Test
+  fun registerButton_isDisplayed() {
+    composeTestRule.setContent {
+      RegisterScreen(viewModel = createMockViewModel(), navigationActions = mockNavigationActions())
+    }
+    composeTestRule.onNodeWithTag("register_button").assertIsDisplayed()
+  }
+
+  @Test
+  fun backButton_isDisplayed() {
+    composeTestRule.setContent {
+      RegisterScreen(viewModel = createMockViewModel(), navigationActions = mockNavigationActions())
+    }
+    composeTestRule.onNodeWithTag("back_button").assertIsDisplayed()
+  }
 }
