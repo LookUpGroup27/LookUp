d684ae9ea65b10b97c36f4ae9f0dbfaec1e83316
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-28 03:54:03 +0100

    feat: implement the login firestore repository to handle everything firebase storage related

--- app/src/main/java/com/github/lookupgroup27/lookup/model/login/LoginRepositoryFirestore.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/login/LoginRepositoryFirestore.kt
@@ -1,4 +1,12 @@
 package com.github.lookupgroup27.lookup.model.login
 
-class LoginRepositoryFirestore {
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.tasks.await
+
+class LoginRepositoryFirestore(private val auth: FirebaseAuth = FirebaseAuth.getInstance()) :
+    LoginRepository {
+
+  override suspend fun loginUser(email: String, password: String) {
+    auth.signInWithEmailAndPassword(email, password).await()
+  }
 }
