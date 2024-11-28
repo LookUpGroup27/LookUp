680678d679ce54ca52b70ff7b485288f132d3136
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-29 00:26:14 +0100

    refactor: add documentation for the login repository

--- app/src/main/java/com/github/lookupgroup27/lookup/model/login/LoginRepository.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/login/LoginRepository.kt
@@ -1,5 +1,15 @@
 package com.github.lookupgroup27.lookup.model.login
 
+/**
+ * Interface defining the contract for login operations. Implementations of this interface should
+ * handle the logic for authenticating a user.
+ */
 interface LoginRepository {
+  /**
+   * Logs in a user with the provided email and password.
+   *
+   * @param email The user's email address.
+   * @param password The user's password.
+   */
   suspend fun loginUser(email: String, password: String)
 }
