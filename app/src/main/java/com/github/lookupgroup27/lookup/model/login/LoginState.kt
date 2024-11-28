cc6cb6e1b9235246dd47c935c57393735ee68e33
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-29 00:27:26 +0100

    refactor: add documentation for the LoginState.kt

--- app/src/main/java/com/github/lookupgroup27/lookup/model/login/LoginState.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/login/LoginState.kt
@@ -1,3 +1,9 @@
 package com.github.lookupgroup27.lookup.model.login
 
+/**
+ * Represents the state of the login screen, containing user input values for email and password.
+ *
+ * @param email The email entered by the user (default is an empty string).
+ * @param password The password entered by the user (default is an empty string).
+ */
 data class LoginState(val email: String = "", val password: String = "")
