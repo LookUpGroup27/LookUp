6e18d7239c582a2e919f1c00f6c24c7c0cd8c9a6
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-11-28 03:58:44 +0100

    refactor: create new file components to write the composables used in the login screen code

--- app/src/main/java/com/github/lookupgroup27/lookup/ui/login/components/CustomOutlinedTextField.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/login/components/CustomOutlinedTextField.kt
@@ -1,4 +1,33 @@
 package com.github.lookupgroup27.lookup.ui.login.components
 
-class CustomOutlinedTextField {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomOutlinedTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    testTag: String
+) {
+  OutlinedTextField(
+      value = value,
+      onValueChange = onValueChange,
+      label = { Text(label, color = Color.White) },
+      modifier = modifier.fillMaxWidth().testTag(testTag),
+      textStyle = MaterialTheme.typography.bodyLarge.copy(color = Color.White),
+      colors =
+          TextFieldDefaults.outlinedTextFieldColors(
+              focusedLabelColor = Color.White,
+              unfocusedLabelColor = Color.Gray,
+              focusedBorderColor = Color.White,
+              unfocusedBorderColor = Color.Gray,
+              cursorColor = Color.White))
 }
