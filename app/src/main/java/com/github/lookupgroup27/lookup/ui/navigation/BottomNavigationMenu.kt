f42de79707cc8685a75f25b4beec8fc746dc5232
raniabbrk <rania.boubrik@epfl.ch>
2024-12-01 15:35:39 +0100

    fix: prevent feed navigation for logged-out users Updated the `BottomNavigationMenu` to include a login check and display a toast message when a logged-out user attempts to navigate to the feed.

--- app/src/main/java/com/github/lookupgroup27/lookup/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/navigation/BottomNavigationMenu.kt
@@ -1,5 +1,6 @@
 package com.github.lookupgroup27.lookup.ui.navigation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
@@ -18,8 +20,10 @@ import androidx.compose.ui.unit.dp
 fun BottomNavigationMenu(
     onTabSelect: (TopLevelDestination) -> Unit,
     tabList: List<TopLevelDestination>,
+    isUserLoggedIn: Boolean,
     selectedItem: String
 ) {
+  val context = LocalContext.current
   NavigationBar(
       modifier = Modifier.fillMaxWidth().height(60.dp).testTag("bottomNavigationMenu"),
       containerColor = Color(0xFF0D1023),
@@ -29,7 +33,15 @@ fun BottomNavigationMenu(
               icon = { Icon(tab.icon, contentDescription = null, tint = Color.White) },
               label = { Text(text = tab.textId, color = Color.White) },
               selected = tab.route == selectedItem,
-              onClick = { if (selectedItem != tab.route) onTabSelect(tab) },
+              onClick = {
+                if (tab.route == Route.FEED && !isUserLoggedIn) {
+                  Toast.makeText(
+                          context, "You need to log in to access the feed.", Toast.LENGTH_LONG)
+                      .show()
+                } else if (selectedItem != tab.route) {
+                  onTabSelect(tab)
+                }
+              },
               modifier = Modifier.clip(shape = RoundedCornerShape(50.dp)).testTag(tab.textId))
         }
       },
