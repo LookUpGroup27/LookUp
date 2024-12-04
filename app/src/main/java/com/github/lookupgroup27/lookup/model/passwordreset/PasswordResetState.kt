53ed7d30a1dee3868f438d339e1a24e65caa6a12
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-12-04 07:44:31 +0100

    style: add documentation

--- app/src/main/java/com/github/lookupgroup27/lookup/model/passwordreset/PasswordResetState.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/passwordreset/PasswordResetState.kt
@@ -1,5 +1,15 @@
 package com.github.lookupgroup27.lookup.model.passwordreset
 
+/**
+ * Represents the UI state for the Password Reset feature.
+ *
+ * This state is used to manage the data and UI elements on the Password Reset screen.
+ *
+ * @property email The email address entered by the user for password reset.
+ * @property isLoading Indicates whether the password reset operation is currently in progress.
+ * @property isSuccess Indicates whether the password reset email was sent successfully.
+ * @property errorMessage Stores any error message to display if the password reset operation fails.
+ */
 data class PasswordResetState(
     val email: String = "",
     val isLoading: Boolean = false,
