b4587157ab55a0f4943f5cb59fe7cd6e110e99b8
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-12-05 15:41:57 +0100

    docs: add detailed documentation to RegisterRepository interface

--- app/src/main/java/com/github/lookupgroup27/lookup/model/register/RegisterRepository.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/register/RegisterRepository.kt
@@ -1,5 +1,19 @@
 package com.github.lookupgroup27.lookup.model.register
 
+/**
+ * Interface defining the contract for user registration repositories.
+ *
+ * Implementations of this interface are responsible for handling the registration logic, such as
+ * interacting with authentication services like FirebaseAuth.
+ */
 interface RegisterRepository {
+
+  /**
+   * Registers a new user with the provided email and password.
+   *
+   * @param email The email address of the new user.
+   * @param password The password for the new user.
+   * @throws Exception If an error occurs during the registration process.
+   */
   suspend fun registerUser(email: String, password: String)
 }
