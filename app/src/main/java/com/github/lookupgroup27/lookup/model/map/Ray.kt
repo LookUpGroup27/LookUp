98dd660c9703750b731ed7558d879a7f3902218b
Kenzoud <kenza.oudrhiribenaddach@epfl.ch>
2024-12-09 23:32:44 +0100

    feat(map): add Ray data class for interaction handling

---
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/map/Ray.kt
@@ -0,0 +1,4 @@
+package com.github.lookupgroup27.lookup.model.map
+
+/** Represents a ray in 3D space, defined by an origin and direction. */
+data class Ray(val origin: FloatArray, val direction: FloatArray)
