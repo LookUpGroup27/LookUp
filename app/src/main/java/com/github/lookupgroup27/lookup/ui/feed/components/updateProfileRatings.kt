6a25017d5d548b051e721b6ea3dab5b2a5a420aa
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-12-12 20:56:02 +0100

    refactor: created a new composable that updates the profile ratings that will be used in the feed screen

--- app/src/main/java/com/github/lookupgroup27/lookup/ui/feed/components/updateProfileRatings.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/feed/components/updateProfileRatings.kt
@@ -1,4 +1,20 @@
 package com.github.lookupgroup27.lookup.ui.feed.components
 
-class updateProfileRatings {
+import com.github.lookupgroup27.lookup.model.profile.UserProfile
+
+fun updateProfileRatings(
+    currentProfile: UserProfile?,
+    postUid: String,
+    starsCount: Int,
+    username: String,
+    bio: String,
+    email: String
+): UserProfile {
+  val updatedRatings =
+      currentProfile?.ratings?.toMutableMap()?.apply { this[postUid] = starsCount }
+          ?: mutableMapOf(postUid to starsCount)
+
+  return currentProfile?.copy(
+      username = username, bio = bio, email = email, ratings = updatedRatings)
+      ?: UserProfile(username = username, bio = bio, email = email, ratings = updatedRatings)
 }
