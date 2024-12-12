14bf46b9f622027896d4bc25d5189013632aa935
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-12-12 20:55:29 +0100

    refactor: created a new composable to update the posts

--- app/src/main/java/com/github/lookupgroup27/lookup/ui/feed/components/calculatePostUpdates.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/ui/feed/components/calculatePostUpdates.kt
@@ -1,4 +1,21 @@
 package com.github.lookupgroup27.lookup.ui.feed.components
 
-class calculatePostUpdates {
+import com.github.lookupgroup27.lookup.model.post.Post
+
+fun calculatePostUpdates(post: Post, userEmail: String, starsCount: Int, oldStarCounts: Int): Post {
+  val isReturningUser = post.ratedBy.contains(userEmail)
+  val newStarsCount =
+      if (isReturningUser) {
+        post.starsCount - oldStarCounts + starsCount
+      } else {
+        post.starsCount + starsCount
+      }
+  val newUsersNumber = if (isReturningUser) post.usersNumber else post.usersNumber + 1
+  val newAvg = newStarsCount.toDouble() / newUsersNumber
+
+  return post.copy(
+      averageStars = newAvg,
+      starsCount = newStarsCount,
+      usersNumber = newUsersNumber,
+      ratedBy = if (!isReturningUser) post.ratedBy + userEmail else post.ratedBy)
 }
