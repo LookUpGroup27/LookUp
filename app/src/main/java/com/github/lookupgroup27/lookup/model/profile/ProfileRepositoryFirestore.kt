b6911c071ca0261e93e895dcaa3c38ccf75a1111
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-12-19 19:13:25 +0100

    feat: make the necessary changes to have unique usernames in the profile

--- app/src/main/java/com/github/lookupgroup27/lookup/model/profile/ProfileRepositoryFirestore.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/profile/ProfileRepositoryFirestore.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.github.lookupgroup27.lookup.model.register.UsernameAlreadyExistsException
 
 data class UserProfile(
     val username: String = " ",
@@ -18,124 +19,139 @@ class ProfileRepositoryFirestore(
     private val auth: FirebaseAuth
 ) : ProfileRepository {
 
-  // private val auth = FirebaseAuth.getInstance()
-  private val collectionPath = "users"
-  private val usersCollection = db.collection(collectionPath)
+    private val collectionPath = "users"
+    private val usersCollection = db.collection(collectionPath)
 
-  override fun init(onSuccess: () -> Unit) {
-    auth.addAuthStateListener {
-      if (it.currentUser != null) {
-        onSuccess()
-      }
+    override fun init(onSuccess: () -> Unit) {
+        auth.addAuthStateListener {
+            if (it.currentUser != null) {
+                onSuccess()
+            }
+        }
     }
-  }
 
-  override fun getUserProfile(onSuccess: (UserProfile?) -> Unit, onFailure: (Exception) -> Unit) {
-    val userId = auth.currentUser?.uid
-    if (userId != null) {
-      usersCollection.document(userId).get().addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-          val profile = task.result?.toObject(UserProfile::class.java)
-          onSuccess(profile)
+    override fun getUserProfile(onSuccess: (UserProfile?) -> Unit, onFailure: (Exception) -> Unit) {
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            usersCollection.document(userId).get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val profile = task.result?.toObject(UserProfile::class.java)
+                    onSuccess(profile)
+                } else {
+                    task.exception?.let {
+                        Log.e("ProfileRepositoryFirestore", "Error getting user profile", it)
+                        onFailure(it)
+                    }
+                }
+            }
         } else {
-          task.exception?.let {
-            Log.e("ProfileRepositoryFirestore", "Error getting user profile", it)
-            onFailure(it)
-          }
+            onSuccess(null) // No logged-in user
         }
-      }
-    } else {
-      onSuccess(null) // No logged-in user
     }
-  }
 
-  override fun updateUserProfile(
-      profile: UserProfile,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit
-  ) {
-    val userId = auth.currentUser?.uid
-    if (userId != null) {
-      performFirestoreOperation(usersCollection.document(userId).set(profile), onSuccess, onFailure)
-    } else {
-      onFailure(Exception("User not logged in"))
-    }
-  }
-
-  override fun deleteUserProfile(
-      profile: UserProfile,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit
-  ) {
-    val userId = auth.currentUser?.uid
-    if (userId != null) {
-      performFirestoreOperation(usersCollection.document(userId).delete(), onSuccess, onFailure)
-    } else {
-      onFailure(Exception("User not logged in"))
-    }
-  }
-
-  override fun logoutUser() {
-    auth.signOut()
-  }
-
-  override fun saveSelectedAvatar(
-      userId: String,
-      avatarId: Int?,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit
-  ) {
-    val userDocument = usersCollection.document(userId)
-    userDocument
-        .get()
-        .addOnSuccessListener { document ->
-          if (!document.exists()) {
-            // Create a default profile with the selected avatar
-            val defaultProfile = mapOf("selectedAvatar" to avatarId)
-            userDocument
-                .set(defaultProfile)
-                .addOnSuccessListener { onSuccess() }
-                .addOnFailureListener { exception -> onFailure(exception) }
-          } else {
-            // Update the existing profile with the selected avatar
-            userDocument
-                .update("selectedAvatar", avatarId)
-                .addOnSuccessListener { onSuccess() }
-                .addOnFailureListener { exception -> onFailure(exception) }
-          }
+    override fun updateUserProfile(
+        profile: UserProfile,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            // Check if the desired username is already taken by another user
+            usersCollection.whereEqualTo("username", profile.username).get()
+                .addOnSuccessListener { querySnapshot ->
+                    val isUsernameTaken = querySnapshot.documents.any { it.id != userId }
+                    if (isUsernameTaken) {
+                        onFailure(UsernameAlreadyExistsException("Username '${profile.username}' is already in use."))
+                    } else {
+                        // Proceed to update the user profile
+                        usersCollection.document(userId).set(profile)
+                            .addOnSuccessListener { onSuccess() }
+                            .addOnFailureListener { exception -> onFailure(exception) }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("ProfileRepositoryFirestore", "Error checking username uniqueness", exception)
+                    onFailure(exception)
+                }
+        } else {
+            onFailure(Exception("User not logged in"))
         }
-        .addOnFailureListener { exception -> onFailure(exception) }
-  }
-
-  override fun getSelectedAvatar(
-      userId: String,
-      onSuccess: (Int?) -> Unit,
-      onFailure: (Exception) -> Unit
-  ) {
-    val userDocument = usersCollection.document(userId)
-    userDocument
-        .get()
-        .addOnSuccessListener { document ->
-          val avatarId = document.getLong("selectedAvatar")?.toInt()
-          onSuccess(avatarId)
-        }
-        .addOnFailureListener { exception -> onFailure(exception) }
-  }
-
-  private fun performFirestoreOperation(
-      task: Task<Void>,
-      onSuccess: () -> Unit,
-      onFailure: (Exception) -> Unit
-  ) {
-    task.addOnCompleteListener { result ->
-      if (result.isSuccessful) {
-        onSuccess()
-      } else {
-        result.exception?.let {
-          Log.e("ProfileRepositoryFirestore", "Error performing Firestore operation", it)
-          onFailure(it)
-        }
-      }
     }
-  }
+
+    override fun deleteUserProfile(
+        profile: UserProfile,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            performFirestoreOperation(usersCollection.document(userId).delete(), onSuccess, onFailure)
+        } else {
+            onFailure(Exception("User not logged in"))
+        }
+    }
+
+    override fun logoutUser() {
+        auth.signOut()
+    }
+
+    override fun saveSelectedAvatar(
+        userId: String,
+        avatarId: Int?,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val userDocument = usersCollection.document(userId)
+        userDocument
+            .get()
+            .addOnSuccessListener { document ->
+                if (!document.exists()) {
+                    // Create a default profile with the selected avatar
+                    val defaultProfile = mapOf("selectedAvatar" to avatarId)
+                    userDocument
+                        .set(defaultProfile)
+                        .addOnSuccessListener { onSuccess() }
+                        .addOnFailureListener { exception -> onFailure(exception) }
+                } else {
+                    // Update the existing profile with the selected avatar
+                    userDocument
+                        .update("selectedAvatar", avatarId)
+                        .addOnSuccessListener { onSuccess() }
+                        .addOnFailureListener { exception -> onFailure(exception) }
+                }
+            }
+            .addOnFailureListener { exception -> onFailure(exception) }
+    }
+
+    override fun getSelectedAvatar(
+        userId: String,
+        onSuccess: (Int?) -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val userDocument = usersCollection.document(userId)
+        userDocument
+            .get()
+            .addOnSuccessListener { document ->
+                val avatarId = document.getLong("selectedAvatar")?.toInt()
+                onSuccess(avatarId)
+            }
+            .addOnFailureListener { exception -> onFailure(exception) }
+    }
+
+    private fun performFirestoreOperation(
+        task: Task<Void>,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        task.addOnCompleteListener { result ->
+            if (result.isSuccessful) {
+                onSuccess()
+            } else {
+                result.exception?.let {
+                    Log.e("ProfileRepositoryFirestore", "Error performing Firestore operation", it)
+                    onFailure(it)
+                }
+            }
+        }
+    }
 }
