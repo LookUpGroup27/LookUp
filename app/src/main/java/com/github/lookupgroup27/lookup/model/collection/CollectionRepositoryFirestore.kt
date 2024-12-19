921b58a984cb9d505571b930dc6b82ea686bc972
Rihab <rihbelmekki@gmail.com>
2024-12-19 10:59:00 +0100

    docs: add missing documentation on CollectionRepositoryFirestore.kt

--- app/src/main/java/com/github/lookupgroup27/lookup/model/collection/CollectionRepositoryFirestore.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/collection/CollectionRepositoryFirestore.kt
@@ -5,6 +5,14 @@ import com.github.lookupgroup27.lookup.model.post.Post
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
+/**
+ * CollectionRepositoryFirestore fetches user-specific posts from Firestore. It queries the "posts"
+ * collection and filters by the currently authenticated user's username. It returns a list of
+ * [Post] objects, enabling the UI to display the user's personal collection.
+ *
+ * @param db The FirebaseFirestore instance used to access firestore data.
+ * @param auth The FirebaseAuth instance used for user authentication.
+ */
 class CollectionRepositoryFirestore(
     private val db: FirebaseFirestore,
     private val auth: FirebaseAuth
@@ -13,6 +21,12 @@ class CollectionRepositoryFirestore(
   private val collection = db.collection("posts")
   private val tag = "CollectionRepositoryFirestore"
 
+  /**
+   * Initializes the repository by checking the authentication state of the user. If the user is
+   * authenticated, the provided [onSuccess] callback is called.
+   *
+   * @param onSuccess A callback to execute when the user is authenticated.
+   */
   override fun init(onSuccess: () -> Unit) {
     auth.addAuthStateListener {
       if (it.currentUser != null) {
@@ -21,6 +35,12 @@ class CollectionRepositoryFirestore(
     }
   }
 
+  /**
+   * Retrieves a list of image URLs associated with the authenticated user's email from Firebase
+   * Storage. If the user is not authenticated or if an error occurs, an empty list is returned.
+   *
+   * @return A list of image URLs associated with the user's email.
+   */
   override fun getUserPosts(onSuccess: (List<Post>?) -> Unit, onFailure: (Exception) -> Unit) {
     val userMail = auth.currentUser?.email
     if (userMail == null) {
