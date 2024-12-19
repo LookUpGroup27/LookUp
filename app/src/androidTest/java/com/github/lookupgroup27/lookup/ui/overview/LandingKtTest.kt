1834710731a0cc60b532fe16835f7f991511ccdb
raniabbrk <rania.boubrik@epfl.ch>
2024-12-19 14:55:10 +0100

    test: Add tests for offline mode handling in LandingScreen
    Added tests to verify the correct offline. Tests include preventing
    navigation to the map when the user is offline.

--- app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/LandingKtTest.kt
+++ app/src/androidTest/java/com/github/lookupgroup27/lookup/ui/overview/LandingKtTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
@@ -13,6 +14,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import com.github.lookupgroup27.lookup.ui.navigation.NavigationActions
 import com.github.lookupgroup27.lookup.ui.navigation.Screen
+import com.github.lookupgroup27.lookup.utils.TestNetworkUtils.simulateOnlineMode
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -20,6 +22,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.never
+
 
 @RunWith(AndroidJUnit4::class)
 class LandingKtTest {
@@ -53,6 +57,9 @@ class LandingKtTest {
 
   @Test
   fun backgroundIsClickableAndNavigatesToMap() {
+
+    //simulate online mode
+    simulateOnlineMode(true)
     composeTestRule.setContent { LandingScreen(mockNavigationActions) }
 
     // Verify that the background image is displayed and clickable
@@ -114,6 +121,26 @@ class LandingKtTest {
 
     // Reset orientation to portrait after the test
     resetOrientation()
+  }
+
+  @Test
+  fun testBackgroundClickDoesNotNavigateToMapWhenOffline() {
+    // Simulate offline mode
+    simulateOnlineMode(false)
+
+    composeTestRule.setContent { LandingScreen(mockNavigationActions) }
+
+    // Verify the background is clickable
+    composeTestRule.onNodeWithTag("LandingScreen").assertHasClickAction()
+
+    // Perform a click action on the background
+    composeTestRule.onNodeWithTag("LandingScreen").performClick()
+
+    // Wait for the UI to settle after the click
+    composeTestRule.waitForIdle()
+
+    // Verify that navigation to the Sky Map is never triggered
+    verify(mockNavigationActions, never()).navigateTo(Screen.SKY_MAP)
   }
 
   private fun setLandscapeOrientation() {
