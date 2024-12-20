46b1b02514bb45cf42d043169b88252f7caa2505
Othmane Bencheqroun <othmane@MacBook-Air-Oth.local>
2024-12-20 01:49:47 +0100

    refactor: change the profile repository to make the usernames unique

--- app/src/main/java/com/github/lookupgroup27/lookup/model/profile/ProfileRepository.kt
+++ app/src/main/java/com/github/lookupgroup27/lookup/model/profile/ProfileRepository.kt
@@ -1,3 +1,4 @@
+// File: ProfileRepository.kt
 package com.github.lookupgroup27.lookup.model.profile
 
 interface ProfileRepository {
@@ -19,4 +20,13 @@ interface ProfileRepository {
   )
 
   fun getSelectedAvatar(userId: String, onSuccess: (Int?) -> Unit, onFailure: (Exception) -> Unit)
+
+  /**
+   * Checks if the given username is already taken by another user.
+   *
+   * @param username The username to check for uniqueness.
+   * @param onResult Callback with true if the username is taken, false otherwise.
+   * @param onFailure Callback with an exception if the operation fails.
+   */
+  fun isUsernameTaken(username: String, onResult: (Boolean) -> Unit, onFailure: (Exception) -> Unit)
 }
